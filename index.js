bf982d83b9b9519252aa660471595646f234416b
EdgarBadillo <80171025+EdgarBadillo@users.noreply.github.com>
2021-08-10 22:57:43 -0500

    accordeon added

---
+++ index.js
@@ -0,0 +1,17 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        // toggle btw hiding and showing
+        let panel = this.nextElementSibling;
+        if(panel.style.display === "block"){
+            panel.style.display = "none";
+        }else{
+            panel.style.display = "block";  
+        }  
+    });
+}
